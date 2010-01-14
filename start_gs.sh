5e651d31218ea112e0da7f4c8dab744e720fe6d5
daemonize <root@static.3.108.46.78.clients.your-server.de>
2010-01-14 18:54:16 +0100

    removed more old files

--- start_gs.sh
+++
@@ -1,1 +0,0 @@
-echo '{"command":"start", "path":"/home/gameserver/admin/25500/", "port":25500, "game":"counter-strike", "install":"false", "params":["-game", "cstrike", "+map", "de_dust2", "-maxplayer", "12", "-port", "25500", "-tickrate", "100", "+hostname", "namevomserver", "+rcon_password", "kuchen", "-autoupdate"]}' > /var/www/virtualHosts/catepo.net/wi/socket
